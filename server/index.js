a60be7beef18b93acba9550a5864c68c8226fdc1
GaetanKarst <gaetan.karst@gmail.com>
2021-05-05 13:07:32 +0900

    attempt for deployment

--- server/index.js
+++ server/index.js
@@ -3,26 +3,31 @@ require("dotenv").config();
 // const typeDefs = require("./schema");
 // const resolver = require("./resolver");
 const db = require('../knexfile');
-const express = require('express');
-const server = express();
+const express = require('express')
+const path = require('path');
 
-server.use("/", express.static("build"));
+const server = express();
+if (__dirname.slice(-6) === '/build') { 
+  server.use(express.static(__dirname))
+} else { 
+  server.use(express.static('build'))
+}
 
 // const server = new ApolloServer({ typeDefs, resolver });
 
 const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+// (async () => {
+//   try {
+//     console.log("Running migrations");
+//       // await db.migrate.latest();
+//     // await console.log("Running seeds");
+//     // await seedDatabase();
 
-(async () => {
-  try {
-    console.log("Running migrations");
-      await db.migrate.latest();
-    // await console.log("Running seeds");
-    // await seedDatabase();
-
-    console.log("Starting express");
-    server.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
-  } catch (err) {
-    console.error("Error starting app!", err);
-    process.exit(-1);
-  }
-})();
+//     console.log("Starting express");
+//     server.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+//   } catch (err) {
+//     console.error("Error starting app!", err);
+//     process.exit(-1);
+//   }
+// })();
