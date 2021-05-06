f91cf8595920e4b815e8f2f371099b832f60729c
GaetanKarst <gaetan.karst@gmail.com>
2021-05-06 20:47:47 +0900

    another attempt for deployment

--- server/index.js
+++ server/index.js
@@ -10,13 +10,14 @@ const server = express();
 server.use(express.static(path.join(__dirname, '../build')));
  
 // const server = new ApolloServer({ typeDefs, resolver });
+server.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/build/index.html'))
+})
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 
-server.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/build/index.html'))
-})
+
 
 // (async () => {
 //   try {
